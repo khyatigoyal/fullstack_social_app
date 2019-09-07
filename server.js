fda100f580de7cdea90a9b21784833a08d7d65a2
Khyati Goyal <17uec065@lnmiit.ac.in>
2019-09-07 16:40:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('APi Running!'));
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
